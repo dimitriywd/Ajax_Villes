dba66e646f7d27a390b752965dfe3b6d8f21d08b
Dimitri <dimitri.g@yeswedev.fr>
2019-03-01 16:56:48 +0100

    Commit initial

---
+++ exo1-tva/main.js
@@ -0,0 +1,22 @@
+const TVA = 20;
+let montant;
+let resultat;
+
+
+window.alert(c);
+
+window.alert(prenom);
+
+console.log(c);
+age=parseInt(window.prompt('Quel âge as tu?'));
+
+console.log(typeof age);
+
+if(age>=18)
+{
+    console.log(prenom + ' Tu peux rentrer');
+}
+else
+{
+    console.log(prenom + ' Tu ne rentres pas');
+}
---
+++ js1-1/main.js
@@ -0,0 +1,24 @@
+let a = 10;
+let b = 20;
+let c = a+b;
+let prenom = ('Dimitri')
+
+let age;
+
+window.alert(c);
+
+window.alert(prenom);
+
+console.log(c);
+age=parseInt(window.prompt('Quel âge as tu?'));
+
+console.log(typeof age);
+
+if(age>=18)
+{
+    console.log(prenom + ' Tu peux rentrer');
+}
+else
+{
+    console.log(prenom + ' Tu ne rentres pas');
+}
